5485492b96e9bdc317a8ff3867a2bf2f115292b0
ProfessorVishal <107393840+CallMeProfessor127001@users.noreply.github.com>
2024-02-15 12:51:23 +0000

    Update DEBIAN_DEPENDENCIES.sh

--- DEBIAN_DEPENDENCIES.sh
+++ DEBIAN_DEPENDENCIES.sh
@@ -6,6 +6,9 @@ sudo apt-get dist-upgrade
 sudo apt-get autoremove
 sudo apt-get clean
 
-
+sudo apt-get install figlet
+sudo apt-get install cowsay
+sudo apt-get install lolcat
+sudo apt-get install toilet
 sudo apt-get install tshark
 sudo apt-get install wireshark
