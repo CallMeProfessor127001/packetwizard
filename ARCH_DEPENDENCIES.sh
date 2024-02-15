98555d5cb7bc7094195e4e7f30ce3a6550bd6328
ProfessorVishal <107393840+CallMeProfessor127001@users.noreply.github.com>
2024-02-15 12:32:41 +0000

    Add files via upload

---
+++ ARCH_DEPENDENCIES.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+pacman -Syy
+pacman -Syu
+pacman -S wireshark-cli
+pacman -S wireshark-qt
---
+++ DEBIAN_DEPENDENCIES.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
+sudo apt-get clean
+
+
+sudo apt-get install tshark
+sudo apt-get install wireshark
